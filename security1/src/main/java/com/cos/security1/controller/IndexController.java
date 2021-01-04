84e61fd3c8fbd6e758eb9e90f58399174481e391
eunsil <atheung1@naver.com>
2021-01-05 02:54:24 +0900

    웹 시큐리티 활성화, user 테이블 생성

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,10 +2,10 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 return함
 public class IndexController {
-
 	// localhost:8080/
 	// localhost:8080
 	@GetMapping({"","/"})
@@ -15,5 +15,36 @@ public class IndexController {
 		//                 	: application.yml에서 설정(생략 가능)
 		return "index"; // src/main/resources/templates/index.mustache를 찾음
 		// view resolver를 오버라이딩 해서 index.html 파일과 매핑되도록 함
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	// spring security의 login 창이 뜸	-> SecurityConfig 파일 생성 후 작동 안 함
+	@GetMapping("/login")
+	public String login() {
+		return "loginForm";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨";
 	}
 }
